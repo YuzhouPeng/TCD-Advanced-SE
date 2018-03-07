5dc5d4ed17d736439b3a573caac60b530e878be1
PointerFLY <linghao.michael.ma@gmail.com>
2018-03-07 16:54:51 +0000

    Delete custom user model

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,10 +1,1 @@
 from django.db import models
-
-
-class User(models.Model):
-    name = models.CharField(max_length=128, primary_key=True)
-    create_time = models.DateTimeField(auto_now_add=True)
-    password = models.CharField(max_length=128)
-
-    def __str__(self):
-        return self.name

d00fadd4ff84cfc907f62260b716f9f00e0b723d
Jiongxu Hou <jiongxuhou@gmail.com>
2018-03-13 18:16:04 +0000

    bike marker and info window done

--- dashboard/static/js/map_controller.js
+++ dashboard/static/js/map_controller.js
@@ -5,22 +5,55 @@ import DataRetriever from "./data_retriever.js";
 export default class MapController {
 
     constructor() {
-        let canvas = document.getElementById('map');
-        let options = {
+        this.canvas = document.getElementById('map');
+        this.options = {
             center: new google.maps.LatLng(53.343507, -6.253920),
             zoom: 16
         };
-        this.map = new google.maps.Map(canvas, options);
+        this.map = new google.maps.Map(this.canvas, this.options);
 
         this.addListeners();
     }
 
     addListeners() {
+        let that = this;
         addEventListener(DataRetriever.DATA_UPDATED_EVENT, function (e) {
-            alert(e.bikeRealtime);
-            alert(e.busRealtime);
-            alert(e.busStations);
-            // update map markers
+            parseToJson(e);
+            for (let i = 0; i < e.bikeRealtime.length; i++) {
+                let contentString = '<div id="content">' +
+                    '<div id="siteNotice">' + '</div>' +
+                    '<h1 id="firstHeading" class="firstHeading">' +
+                    e.bikeRealtime[i].name + '<br>' +
+                    'Available bikes: ' + e.bikeRealtime[i].bike_available + '<br>' +
+                    'Free stands: ' + (e.bikeRealtime[i].bike_stands - e.bikeRealtime[i].bike_available) +
+                    '</h1>';
+
+                createMarker(e.bikeRealtime[i].name, e.bikeRealtime[i].latitude, e.bikeRealtime[i].longitude, contentString)
+            }
+
+            let infoWindow = new google.maps.InfoWindow({});
+
+            function parseToJson(e) {
+                for (let i = 1; i < Object.keys(e).length; i++) {
+                    e[Object.keys(e)[i]] = JSON.parse(e[Object.keys(e)[i]]);
+                    console.log(e)
+                }
+            }
+
+            function createMarker(title, lat, lon, contentString) {
+                let marker = new google.maps.Marker({
+                    position: new google.maps.LatLng(lat, lon),
+                    map: that.map,
+                    title: title,
+                    contentString: contentString,
+                });
+                google.maps.event.addListener(marker, 'click', function () {
+                    infoWindow.setContent(marker.contentString);
+                    infoWindow.open(map, marker);
+                    that.map.panTo(marker.getPosition())
+                });
+            }
+
         });
     }
 

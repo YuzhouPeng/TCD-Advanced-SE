fc248a1042110917f749147eef095326bf60ade9
PointerFLY <linghao.michael.ma@gmail.com>
2018-03-31 16:37:12 +0100

    Change Monitor table columns

--- frontend/src/components/Monitor.vue
+++ frontend/src/components/Monitor.vue
@@ -1,48 +1,50 @@
 <template>
-  <div id="monitor" class="container">
-    <div class="col-md-5 mb-3">
-      <label class="select-label">Bus station emission alert threshold</label>
-      <select class="custom-select d-block w-100" required>
-        <option v-for="option in busOptions" :key="option.id" :value="option">{{option}}</option>
-      </select>
+  <div id="monitor">
+    <div class="container">
+      <div class="col-md-5 mb-3 select-div">
+        <label class="select-label">Bus station emission alert threshold</label>
+        <select class="custom-select d-block w-100" required>
+          <option v-for="option in busThresholdOptions" :key="option.id" :value="option">{{option}}</option>
+        </select>
+      </div>
+
+      <div class="col-md-5 mb-3 select-div">
+        <label class="select-label">Bike station available bike alert threshold</label>
+        <select class="custom-select d-block w-100" required>
+          <option v-for="option in bikeThresholdOptions" :key="option.id" :value="option">{{option}}</option>
+        </select>
+      </div>
+
+      <div class="col-md-5 mb-3 select-div">
+        <label class="select-label">Type: Bike or Bus</label>
+        <select class="custom-select d-block w-100" required>
+          <option v-for="option in types" :key="option.id" :value="option">{{option}}</option>
+        </select>
+      </div>
+
+      <div class="col-md-5 mb-3 select-div">
+        <label class="select-label">Choose from available times</label>
+        <select class="custom-select d-block w-100" required>
+          <option v-for="option in times" :key="option.id" :value="option">{{option}}</option>
+        </select>
+      </div>
+
+      <table class="table table-bordered">
+        <thead class="thead-light">
+        <tr><th v-for="head in headTitles" :key="head.id">{{head}}</th></tr>
+        </thead>
+        <tbody>
+        <tr v-for="stop in alertStops" :key="stop.id">
+          <th>{{stop.time}}</th>
+          <th>{{stop.type}}</th>
+          <td>{{stop.id}}</td>
+          <td>{{stop.name}}</td>
+          <td>{{stop.emissionLevel}}</td>
+          <td>{{stop.availableBike}}</td>
+        </tr>
+        </tbody>
+      </table>
     </div>
-
-    <table class="table table-bordered">
-      <thead class="thead-light">
-      <tr><th v-for="head in busHeads" :key="head.id">{{head}}</th></tr>
-      </thead>
-      <tbody>
-      <tr v-for="stop in alertedBusStops" :key="stop.id">
-        <th scope="row">{{stop.date}}</th>
-        <td>{{stop.id}}</td>
-        <td>{{stop.name}}</td>
-        <td>{{stop.emissionLevel}}</td>
-      </tr>
-      </tbody>
-    </table>
-
-    <div id="mid-divider"></div>
-
-    <div class="col-md-5 mb-3">
-      <label class="select-label">Bike station available bike alert threshold</label>
-      <select class="custom-select d-block w-100" required>
-        <option v-for="option in bikeOptions" :key="option.id" :value="option">{{option}}</option>
-      </select>
-    </div>
-
-    <table class="table table-bordered">
-      <thead class="thead-light">
-      <tr><th v-for="head in bikeHeads" :key="head.id">{{head}}</th></tr>
-      </thead>
-      <tbody>
-      <tr v-for="stop in alertedBikeStops" :key="stop.id">
-        <th scope="row">{{stop.date}}</th>
-        <td>{{stop.id}}</td>
-        <td>{{stop.name}}</td>
-        <td>{{stop.availableCount}}</td>
-      </tr>
-      </tbody>
-    </table>
   </div>
 </template>
 
@@ -51,45 +53,48 @@ export default {
   name: 'monitor',
   data () {
     return {
-      busHeads: ['Date', 'Stop ID', 'Stop Name', 'Emission Level'],
-      bikeHeads: ['Date', 'Stop ID', 'Stop Name', 'Bike Available'],
-      busOptions: [1, 2, 3, 4, 6, 7, 8],
-      bikeOptions: [1, 2, 3, 4, 5, 6, 7, 8],
+      headTitles: ['Time', 'Type', 'Stop ID', 'Stop Name', 'Emission Level', 'Available Bike'],
+      busThresholdOptions: [1, 2, 3, 4, 6, 7, 8],
+      bikeThresholdOptions: [1, 2, 3, 4, 5, 6, 7, 8],
+      types: ['Bus', 'Bike', 'Both'],
+      times: ['18:00', '19:00', '21:00'],
 
-      alertedBusStops: [
+      alertStops: [
         {
-          date: '18:00',
+          time: '18:00',
+          type: 'bus',
           id: '123',
           name: '1213',
-          emissionLevel: 200
+          availableBike: 12,
+          emissionLevel: null
         },
         {
-          date: '18:00',
+          time: '18:00',
+          type: 'bike',
           id: '123',
           name: '1213',
+          availableBike: null,
           emissionLevel: 100
-        }
-      ],
-      alertedBikeStops: [
-        {
-          date: '18:00',
-          id: '123',
-          name: '1213',
-          availableCount: 10
         },
         {
-          date: '18:00',
+          time: '18:00',
+          type: 'bike',
           id: '123',
           name: '1213',
-          availableCount: 12
-        }
-      ],
+          availableBike: null,
+          emissionLevel: 100
+        },
+      ]
     }
   }
 }
 </script>
 
 <style scoped>
+.select-div {
+  display: inline-block;
+}
+
 #monitor {
   overflow-y: scroll;
   width: 100%;
@@ -102,9 +107,5 @@ export default {
 
 .select-label {
   font-size: 1.05em;
-}
-
-#mid-divider {
-  height: 12px;
 }
 </style>

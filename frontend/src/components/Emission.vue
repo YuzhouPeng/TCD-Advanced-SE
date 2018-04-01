7e612bbfdf49befaa8c7893768cbdc6201a7293b
PointerFLY <linghao.michael.ma@gmail.com>
2018-04-01 21:18:38 +0100

    Show stations

--- frontend/src/components/Emission.vue
+++ frontend/src/components/Emission.vue
@@ -1,26 +1,14 @@
 <template>
     <div id="emission">
-    <div id="map" ref="map"></div>
+    <div id="map-emission"></div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'emission',
-  map: null,
   created () {
-    let GoogleMap = require('google-maps')
-    GoogleMap.KEY = 'AIzaSyAN-k2Z0JpTVPPzGkt61YImxgxTf4PUzk0'
-    GoogleMap.LANGUAGE = 'en'
 
-    GoogleMap.load(google => {
-      let canvas = this.$refs.map
-      let options = {
-        center: new google.maps.LatLng(53.343507, -6.253920),
-        zoom: 16
-      }
-      this.map = new google.maps.Map(canvas, options)
-    })
   }
 }
 </script>
@@ -33,7 +21,7 @@ export default {
   height: 100%;
 }
 
-#map {
+#map-emission {
   width: 100%;
   height: 100%;
 }
